0aa1848356bccc0d3e37430ac2f7d4c38df1f182
Galih-Prasetya-kun <63963416+Lukyrafi13@users.noreply.github.com>
2020-04-23 20:43:57 +0700

    Penambahan Activity

---
+++ app/src/main/java/com/example/aplikasidapurresep/tentang_galih_prasetya_widodo.java
@@ -0,0 +1,14 @@
+package com.example.aplikasidapurresep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tentang_galih_prasetya_widodo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang_galih_prasetya_widodo);
+    }
+}

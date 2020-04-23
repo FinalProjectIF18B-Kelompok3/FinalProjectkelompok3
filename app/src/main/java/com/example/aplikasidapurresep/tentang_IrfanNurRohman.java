fa6a471f26ded0846fe16aff71c1bffa43fb3d28
IrfanNurRohman <irfan.noerrochman@gmail.com>
2020-04-23 23:14:50 +0700

    menambahkan activity

---
+++ app/src/main/java/com/example/aplikasidapurresep/tentang_IrfanNurRohman.java
@@ -0,0 +1,14 @@
+package com.example.aplikasidapurresep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tentang_IrfanNurRohman extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang__irfan_nur_rohman);
+    }
+}

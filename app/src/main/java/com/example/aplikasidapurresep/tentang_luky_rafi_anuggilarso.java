26cc5d1ce3cd7dee67331deb1e0284c308dd0b8f
Lukyrafi13 <63963416+Lukyrafi13@users.noreply.github.com>
2020-04-23 18:55:15 +0700

    Penambahan Activity
    Activity Luky Rafi Anuggilarso

---
+++ app/src/main/java/com/example/aplikasidapurresep/tentang_luky_rafi_anuggilarso.java
@@ -0,0 +1,14 @@
+package com.example.aplikasidapurresep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tentang_luky_rafi_anuggilarso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang_luky_rafi_anuggilarso);
+    }
+}

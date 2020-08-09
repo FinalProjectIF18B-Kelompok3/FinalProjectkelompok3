1e965941cd7a6782d40f105622904d53e5953b58
GalihPrasetya-kun <galih.prasta007@gmail.com>
2020-08-09 16:28:28 +0700

    Menambahkan Activity Tambah Resep

--- app/src/main/java/com/example/aplikasidapurresep/AddActivity.java
+++ app/src/main/java/com/example/aplikasidapurresep/AddActivity.java
@@ -1,14 +1,163 @@
 package com.example.aplikasidapurresep;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
 
 public class AddActivity extends AppCompatActivity {
+    ImageView receiptImage;
+    Uri imageUri;
+    Button btnSelectImage;
+    Button btnPost;
+
+    String myUrl = "";
+    StorageTask uploadTask;
+    StorageReference storageReference;
+
+    EditText receiptName;
+    EditText receiptDescription;
+    EditText receiptIngredients;
+    EditText receiptTimeCook;
+    EditText receiptWaysOfMaking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        receiptImage = (ImageView)findViewById(R.id.image_foodimage);
+        receiptName = findViewById(R.id.txt_receipt_name);
+        receiptDescription = findViewById(R.id.txt_description);
+        receiptIngredients = findViewById(R.id.txt_ingredients);
+        receiptTimeCook = findViewById(R.id.txt_timeCook);
+        receiptWaysOfMaking = findViewById(R.id.txt_waysOfMaking);
+
+        storageReference = FirebaseStorage.getInstance().getReference("posts");
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+
+        btnSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.activity()
+                        .setAspectRatio(1,1)
+                        .start(AddActivity.this);
+            }
+        });
+
     }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadImage(){
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Posting");
+        progressDialog.show();
+
+        if (imageUri != null){
+            final StorageReference filereference = storageReference.child(System.currentTimeMillis()
+                    + "." + getFileExtension(imageUri));
+
+            uploadTask = filereference.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if (!task.isSuccessful()){
+                        throw task.getException();
+                    }
+
+                    return filereference.getDownloadUrl();
+                }
+            }).addOnCompleteListener (new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()){
+                        Uri downloadUri = task.getResult();
+                        myUrl = downloadUri.toString();
+
+                        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+
+                        String postid = reference.push().getKey();
+
+                        HashMap<String , Object> hashMap = new HashMap<>();
+                        hashMap.put("postid", postid);
+                        hashMap.put("postimage", myUrl);
+                        hashMap.put("Name Receipt", receiptName.getText().toString());
+                        hashMap.put("Description Receipt", receiptDescription.getText().toString());
+                        hashMap.put("Ingredients", receiptIngredients.getText().toString());
+                        hashMap.put("Time Cook", receiptTimeCook.getText().toString());
+                        hashMap.put("Way Of Making", receiptWaysOfMaking.getText().toString());
+                        hashMap.put("publisher", FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+                        reference.child(postid).setValue(hashMap);
+
+                        progressDialog.dismiss();
+
+                        startActivity(new Intent(AddActivity.this, MainActivity.class));
+                        finish();
+                    } else{
+                        Toast.makeText(AddActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(AddActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Toast.makeText(this, "No Image Selected!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            imageUri = result.getUri();
+
+            receiptImage.setImageURI(imageUri);
+        } else {
+            Toast.makeText(this, "Something gone wrong", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(AddActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+
 }
